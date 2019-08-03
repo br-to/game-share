a19cc8c4afc9d4ca1a7ec06456f43e6c0b147988
raba <barabara0224@gmail.com>
2019-08-03 22:11:43 +0900

    user controller add 2nd

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,26 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def create
+    @user = User.new(users_params)
+    if @user.save
+      flash[:success] = "登録しました"
+      redirect_to @user
+    else
+      flash[:danger] = "登録できませんでした"
+      render "new"
+    end
   end
+
+  private
+
+    def users_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
