0a3fca95889c9d2a3e82dbcb8d8fb2537076a011
raba <barabara0224@gmail.com>
2019-08-05 01:23:28 +0900

    add password validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }
   validates :name, presence: true, length: { maximum: 50 }
   has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
