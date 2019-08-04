7b03bbc7fc707e0709a61b12fa3be6771984ff49
raba <barabara0224@gmail.com>
2019-08-05 01:43:27 +0900

    update session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      log_in(user)
+      redirect_to user, success: "ログインしました"
+    else
+      flash.now[:warning] = "ログインできませんでした"
+      render "new"
+    end
   end
 
   def destroy
+    log_out if logged_in?
+    redirect_to root_url, info: "ログアウトしました"
   end
 end
