75ffed62e51262d994168989ee08ec84fcee9eaa
raba <barabara0224@gmail.com>
2019-08-05 16:24:46 +0900

    fix order

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
   before_save :downcase_email
   validates :email, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }
   validates :name, presence: true, length: { maximum: 50 }
-  has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+  has_secure_password
 
   private
 
