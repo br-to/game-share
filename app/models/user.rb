8a567b148a41b69e091cbe067735103137963819
raba <barabara0224@gmail.com>
2019-08-05 11:11:56 +0900

    add downcase_email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
+  before_save :downcase_email
   validates :email, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }
   validates :name, presence: true, length: { maximum: 50 }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  private
+
+    def downcase_email
+      email.downcase!
+    end
 end
