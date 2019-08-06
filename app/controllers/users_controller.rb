1dcea548d778cd6235a8e2db803ba2a69b6848ba
raba <barabara0224@gmail.com>
2019-08-06 14:17:40 +0900

    add destroy & before action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:show, :edit, :update, :destroy]
+  before_action :ensure_correct_user, only: [:edit, :update]
   def new
     @user = User.new
   end
@@ -32,9 +34,19 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.destroy(params[:id])
+    redirect_to root_url, danger: "ユーザーを削除しました"
+  end
+
   private
 
     def users_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def ensure_correct_user
+      @user = User.find(params[:id])
+      redirect_to(login_url) unless current_user?(@user)
     end
 end
