8445e5dd9b3038d9b79400e6b4e2a3f16d3a65ff
raba <barabara0224@gmail.com>
2019-08-06 23:47:42 +0900

    update user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   skip_before_action :authenticate_user, only: [:new, :create]
-  before_action :ensure_correct_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
     @user = User.new(users_params)
     if @user.save
       log_in(@user)
-      redirect_to @user, success: "登録しました"
+      redirect_to profile_path, success: "登録しました"
     else
       flash.now[:danger] = "登録できませんでした"
       render "new"
@@ -20,28 +20,9 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  def update
-    @user = User.find(params[:id])
-    if @user.update(users_params)
-      redirect_to @user, success: "編集しました"
-    else
-      flash.now[:warning] = "編集できませんでした"
-      render "edit"
-    end
-  end
-
   private
 
     def users_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
-
-    def ensure_correct_user
-      @user = User.find(params[:id])
-      redirect_to(login_url) unless current_user?(@user)
     end
 end
