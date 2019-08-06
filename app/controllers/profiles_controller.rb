6dbf56d20e6571a720ad4fd5c624b0fe43417be4
raba <barabara0224@gmail.com>
2019-08-06 23:49:13 +0900

    update profile controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,15 +1,30 @@
 class ProfilesController < ApplicationController
-  skip_before_action :authenticate_user, only: [:destoy]
-  before_action :set_current_user
+  def show
+    current_user
+  end
+
+  def edit
+    current_user
+  end
+
+  def update
+    current_user
+    if current_user.update(users_params)
+      redirect_to profile_url, success: "更新しました"
+    else
+      flash.now[:warning] = "編集できませんでした"
+      render "edit"
+    end
+  end
 
   def destroy
-    @current_user.destroy
+    current_user.destroy
     redirect_to root_url, success: "ユーザー削除"
   end
 
   private
 
-    def set_current_user
-      @current_user = User.find_by(id: session[:user_id])
+    def users_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
