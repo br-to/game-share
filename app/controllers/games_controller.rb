3f279902a47e06c6609e6cbac15c7f7b96a9895d
raba <barabara0224@gmail.com>
2019-08-08 06:12:03 +0900

    add index

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,19 +1,24 @@
 class GamesController < ApplicationController
-  def new
-    @game = current_user.games.build
+  def index
+    @games = Game.all.order(created_at: :desc)
   end
 
-  def show
+  def new
+    @game = current_user.games.build
   end
 
   def create
     @game = current_user.games.build(game_params)
     if @game.save
-      redirect_to user_path(current_user), success: "登録しました"
+      redirect_to games_path, success: "登録しました"
     else
       flash.now[:warning] = "登録できませんでした"
       render "new"
     end
+  end
+
+  def show
+    @game = Game.find(params[:id])
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
   resources :users, only: [:new, :create, :show]
-  resources :games, only: [:new, :create, :show, :update, :destroy]
+  resources :games, only: [:index, :new, :create, :show, :update, :destroy]
   resource :profile, only: [:show, :edit, :update, :destroy]
 end
