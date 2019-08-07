c1c69f4d3351cbd204bde490e493f89c520ce2c5
raba <barabara0224@gmail.com>
2019-08-07 12:24:30 +0900

    create game-title DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_03_065545) do
+ActiveRecord::Schema.define(version: 2019_08_07_025634) do
+
+  create_table "games", force: :cascade do |t|
+    t.string "title"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["title"], name: "index_games_on_title"
+    t.index ["user_id"], name: "index_games_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
