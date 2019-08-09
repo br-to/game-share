4a5c285f7adc958ac2ab2dab4f1e6e3ed2b5a4ea
raba <barabara0224@gmail.com>
2019-08-10 02:24:43 +0900

    add default_scope

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,4 +2,5 @@ class Micropost < ApplicationRecord
   belongs_to :user
   belongs_to :game
   validates :content, presence: true, length: { maximum: 255 }
+  default_scope -> { order(created_at: :desc) }
 end
