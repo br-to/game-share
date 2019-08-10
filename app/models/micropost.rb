6128d02695467ed027b9d2a0b66d33270c6862b2
raba <barabara0224@gmail.com>
2019-08-11 00:05:01 +0900

    fix Law of Demetar

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,6 +1,8 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   belongs_to :game
+  delegate :name, to: :user, prefix: true
+  delegate :title, to: :game, prefix: true
   validates :content, presence: true, length: { maximum: 255 }
   default_scope -> { order(created_at: :desc) }
 end
