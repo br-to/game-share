5e411f17ad3d4144a3741ace39d238fe2300d622
raba <barabara0224@gmail.com>
2019-08-15 02:44:28 +0900

    define favorited_by?

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,4 +5,8 @@ class Review < ApplicationRecord
   delegate :name, to: :user, prefix: true
   delegate :title, to: :game, prefix: true
   validates :content, presence: true, length: { maximum: 255 }
+
+  def favorited_by?(user)
+    likes.where(user_id: user.id).exists?
+  end
 end
