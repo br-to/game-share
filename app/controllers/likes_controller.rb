13883cda1c862131a3632b0c2e6edd793ee58e55
raba <barabara0224@gmail.com>
2019-08-15 02:44:59 +0900

    add likes controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,7 +1,17 @@
 class LikesController < ApplicationController
   def create
+    @game = Game.find(params[:game_id])
+    @review = Review.find(params[:review_id])
+    @like = current_user.likes.build(game_id: params[:game_id], review_id: params[:review_id])
+    @like.save!
+    redirect_to game_review_url(@game, @review)
   end
 
   def destroy
+    @game = Game.find(params[:game_id])
+    @review = Review.find(params[:review_id])
+    @like = Like.find_by(game_id: params[:game_id], review_id: params[:review_id], user_id: current_user.id)
+    @like.destroy!
+    redirect_to game_review_url(@game, @review)
   end
 end
