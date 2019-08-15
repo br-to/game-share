0096c96aea32f12502e03095568638a4b904d7cf
raba <barabara0224@gmail.com>
2019-08-15 11:38:43 +0900

    add likes_count

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,7 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :game
   has_many :likes, dependent: :destroy
+  has_many :likes_users, through: :likes, source: :user
   delegate :name, to: :user, prefix: true
   delegate :title, to: :game, prefix: true
   validates :content, presence: true, length: { maximum: 255 }
