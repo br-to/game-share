33ddf419c5ee6e5c8ae4fdc36f461a2af55ee43b
raba <barabara0224@gmail.com>
2019-08-15 11:38:57 +0900

    add nice review

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :games, dependent: :destroy
   has_many :reviews, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :likes_review, through: :likes, source: :review
   before_save :downcase_email
   validates :email, presence: true, length: { maximum: 255 }, uniqueness: { case_sensitive: false }
   validates :name, presence: true, length: { maximum: 50 }
