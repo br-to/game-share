7480bb7654f95b2bf2e35f0f40eb732b6b44eaa9
raba <barabara0224@gmail.com>
2019-08-16 21:44:57 +0900

    fix like controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,13 @@
 class LikesController < ApplicationController
   def create
-    @review = Review.find(params[:review_id])
-    @like = current_user.likes.build(review_id: params[:review_id])
-    @like.save!
-    redirect_to review_url(@review)
+    review = Review.find(params[:review_id])
+    like = review.likes.create!(user_id: current_user.id)
+    redirect_to review_url(review)
   end
 
   def destroy
-    @like = Like.find_by(user_id: current_user.id)
-    @like.destroy!
+    like = Like.find_by(user_id: current_user.id)
+    like.destroy!
     redirect_to review_url
   end
 end
