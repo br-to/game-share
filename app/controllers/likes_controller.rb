5a556c4830408d8507121f257088f0729c9815b7
raba <barabara0224@gmail.com>
2019-08-18 23:45:33 +0900

    update like controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,13 @@
 class LikesController < ApplicationController
   def create
     review = Review.find(params[:review_id])
-    like = review.likes.create!(user_id: current_user.id)
-    redirect_to review_url(review)
+    like = current_user.likes.create!(review: review)
+    redirect_to review_url(review.id)
   end
 
   def destroy
-    like = Like.find_by(user_id: current_user.id)
+    like = Like.find(params[:id])
     like.destroy!
-    redirect_to review_url
+    redirect_to review_url(like.review)
   end
 end
