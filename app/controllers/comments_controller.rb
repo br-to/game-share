2dfb56ce6685d3e9fb109a90f9a2bb5463a79a1f
raba <barabara0224@gmail.com>
2019-08-19 15:40:44 +0900

    add create & destroy

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,25 @@
 class CommentsController < ApplicationController
   def create
+    review = Review.find(params[:review_id])
+    comment = review.comments.build(params_comment)
+    comment.user_id = current_user.id
+    if comment.save
+      redirect_to review_url(review.id), success: "コメントしました"
+    else
+      render "reviews/show"
+      flash.now[:warning] = "コメントできません"
+    end
   end
 
   def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy!
+    redirect_to review_url(comment.review), success: "コメントを削除しました"
   end
+
+  private
+
+    def params_comment
+      params.require(:comment).permit(:content)
+    end
 end
