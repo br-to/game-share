701d5d06eb16e3e4824f3e509bd1c240dd2ba072
raba <barabara0224@gmail.com>
2019-08-19 23:11:25 +0900

    add instance & fix order

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,20 +1,20 @@
 class CommentsController < ApplicationController
   def create
-    review = Review.find(params[:review_id])
-    comment = review.comments.build(params_comment)
-    comment.user_id = current_user.id
-    if comment.save
-      redirect_to review_url(review.id), success: "コメントしました"
+    @review = Review.find(params[:review_id])
+    @comment = @review.comments.build(params_comment)
+    @comment.user_id = current_user.id
+    if @comment.save
+      redirect_to review_url(@review), success: "コメントしました"
     else
-      render "reviews/show"
       flash.now[:warning] = "コメントできません"
+      render "reviews/show"
     end
   end
 
   def destroy
-    comment = Comment.find(params[:id])
-    comment.destroy!
-    redirect_to review_url(comment.review), success: "コメントを削除しました"
+    @comment = current_user.comments.find(params[:id])
+    @comment.destroy!
+    redirect_to review_url(@comment.review), success: "コメントを削除しました"
   end
 
   private
