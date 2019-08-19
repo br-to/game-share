e3438cb7f6dad4322ae19f39becbd7681c1fcf48
raba <barabara0224@gmail.com>
2019-08-19 15:41:08 +0900

    add delegate

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :review
+  delegate :name, to: :user, prefix: true
   validates :content, presence: true, length: { maximum: 150 }
 end
