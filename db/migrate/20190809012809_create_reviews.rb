05e95aaddc251134bf8392bb2ef4031ca2119ff5
raba <barabara0224@gmail.com>
2019-08-21 21:59:59 +0900

    fix reviews_table

--- db/migrate/20190809012809_create_reviews.rb
+++ db/migrate/20190809012809_create_reviews.rb
@@ -4,7 +4,7 @@ class CreateReviews < ActiveRecord::Migration[5.2]
       t.text :content
       t.integer :user_id, index: true
       t.integer :game_id, index: true
-      t.string :netabare_digest
+      t.boolean :is_netabare, default: false, null: false
 
       t.timestamps
     end
