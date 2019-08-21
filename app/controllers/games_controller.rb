028dd3613be0c8c9837d9bfbdafb9f267a3de6db
raba <barabara0224@gmail.com>
2019-08-21 22:13:46 +0900

    add where

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -19,7 +19,7 @@ class GamesController < ApplicationController
 
   def show
     @game = Game.find(params[:id])
-    @reviews = @game.reviews.includes(:user, :comments)
+    @reviews = @game.reviews.where(is_netabare: "0").includes(:user, :comments)
   end
 
   def edit
