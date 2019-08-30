403d312974914c202a057dc0b832fa7a0da00966
raba <barabara0224@gmail.com>
2019-08-31 01:36:32 +0900

    add email downcase test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,17 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
+  before do
+    @user = build(:user)
+  end
+
   it "has a valid factory" do
     expect(build(:user)).to be_valid
+  end
+
+  it "email addresses should be saved as lower-case" do
+    @user.save!
+    expect(@user.reload.email).to eq @user.email.downcase
   end
 
   describe "association" do
