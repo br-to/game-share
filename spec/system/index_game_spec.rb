34ea163ccccd0497173ed206d3bf12bee15ab1f7
raba <barabara0224@gmail.com>
2019-09-04 23:55:15 +0900

    delete variable

--- spec/system/index_game_spec.rb
+++ spec/system/index_game_spec.rb
@@ -3,9 +3,9 @@ require "rails_helper"
 describe "ゲーム一覧", type: :system do
   let(:user) { create :user }
   before do
-    game1 = create(:game, title: "pokemon", user: user)
-    game2 = create(:game, title: "mario", user: user)
-    game3 = create(:game, title: "zeruda", user: user)
+    create(:game, title: "pokemon", user: user)
+    create(:game, title: "mario", user: user)
+    create(:game, title: "zeruda", user: user)
   end
 
   it "作成順に並ぶと成功" do
