4f1782fbf8cbd9b3a0b2ce1237f8e52c0d26a743
raba <barabara0224@gmail.com>
2019-09-04 12:15:08 +0900

    dekete instance parameter

--- spec/system/delete_game_spec.rb
+++ spec/system/delete_game_spec.rb
@@ -1,14 +1,14 @@
 require "rails_helper"
 
-describe "ゲームタイトル削除" do
+describe "ゲームタイトル削除", type: :system do
   let(:user) { create :user }
+  let(:game) { create :game, user: user }
   before do
-    @game = create(:game, user: user)
     login user
   end
 
   it "titleの削除成功" do
-    visit game_path(@game)
+    visit game_path(game)
     click_link I18n.t("link.delete_game")
     page.driver.browser.switch_to.alert.accept
     expect(page).to have_content I18n.t("flash.game_destroy_success")
--- spec/system/delete_review_spec.rb
+++ spec/system/delete_review_spec.rb
@@ -1,15 +1,15 @@
 require "rails_helper"
 
-describe "感想・レビュー削除" do
+describe "感想・レビュー削除", type: :system do
   let(:user) { create :user }
   let(:game) { create :game }
+  let(:review) { create :review, game: game, user: user }
   before do
-    @review = create(:review, game: game, user: user)
     login user
   end
 
   it "感想・レビュー削除成功" do
-    visit review_path(@review)
+    visit review_path(review)
     click_link I18n.t("link.delete_review")
     page.driver.browser.switch_to.alert.accept
     expect(page).to have_content I18n.t("flash.review_destroy_success")
