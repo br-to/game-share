a136fba4a533047922aada059a8fe03f2b7a5e9b
raba <barabara0224@gmail.com>
2019-09-04 12:27:57 +0900

    fix like test

--- spec/system/like_spec.rb
+++ spec/system/like_spec.rb
@@ -1,33 +1,27 @@
 require "rails_helper"
 
-describe "感想・レビューをナイスする" do
-  let(:user) { create :user }
-  let(:review) { create :review }
+describe "ナイス機能", type: :system do
   before do
-    login user
-  end
-
-  it "ナイス成功" do
-    visit review_path(review)
-    click_link I18n.t("link.register_like")
-    expect(page).to have_content I18n.t("link.delete_like")
-    expect(page).to have_content I18n.t("message.likes_count", count: 1)
-  end
-end
-
-describe "感想・レビューのナイスを取り消す" do
-  let(:user) { create :user }
-  let(:review) { create :review }
-
-  before do
+    user = create(:user)
+    review = create(:review, user: user)
     create(:like, user: user, review: review)
     login user
-  end
-
-  it "ナイス取り消しに成功" do
     visit review_path(review)
     click_link I18n.t("link.delete_like")
-    expect(page).to have_content I18n.t("link.register_like")
-    expect(page).to have_content I18n.t("message.likes_count", count: 0)
+  end
+
+  describe "感想・レビューをナイスする" do
+    it "ナイス成功" do
+      click_link I18n.t("link.register_like")
+      expect(page).to have_content I18n.t("link.delete_like")
+      expect(page).to have_content I18n.t("message.likes_count", count: 1)
+    end
+  end
+
+  describe "感想・レビューのナイスを取り消す" do
+    it "ナイス取り消しに成功" do
+      expect(page).to have_content I18n.t("link.register_like")
+      expect(page).to have_content I18n.t("message.likes_count", count: 0)
+    end
   end
 end
