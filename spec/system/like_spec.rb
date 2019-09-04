35a1d03f196fc101a08898cf411754634e3466a1
raba <barabara0224@gmail.com>
2019-09-04 23:10:22 +0900

    fix like test

--- spec/system/like_spec.rb
+++ spec/system/like_spec.rb
@@ -1,16 +1,14 @@
 require "rails_helper"
 
 describe "ナイス機能", type: :system do
-  before do
-    user = create(:user)
-    review = create(:review, user: user)
-    create(:like, user: user, review: review)
-    login user
-    visit review_path(review)
-    click_link I18n.t("link.delete_like")
-  end
-
   describe "感想・レビューをナイスする" do
+    before do
+      user = create(:user)
+      review = create(:review, user: user)
+      login user
+      visit review_path(review)
+    end
+
     it "ナイス成功" do
       click_link I18n.t("link.register_like")
       expect(page).to have_content I18n.t("link.delete_like")
@@ -19,7 +17,16 @@ describe "ナイス機能", type: :system do
   end
 
   describe "感想・レビューのナイスを取り消す" do
+    before do
+      user = create(:user)
+      review = create(:review, user: user)
+      create(:like, user: user, review: review)
+      login user
+      visit review_path(review)
+    end
+
     it "ナイス取り消しに成功" do
+      click_link I18n.t("link.delete_like")
       expect(page).to have_content I18n.t("link.register_like")
       expect(page).to have_content I18n.t("message.likes_count", count: 0)
     end
