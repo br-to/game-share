89f45f66d3a84fa7b4ae042f335f5ee1432c3a45
raba <barabara0224@gmail.com>
2019-09-04 13:14:55 +0900

    fix user model test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,22 @@ RSpec.describe User, type: :model do
     expect(build(:user)).to be_valid
   end
 
+  it "email format should be invalid" do
+    addresses = %w[user@foo,com user_at_foo.org example.user@foo.foo@bar_baz.com foo@bar+baz.com]
+    addresses.each do |invalid_address|
+      @user.email = invalid_address
+      expect(@user).not_to be_valid
+    end
+  end
+
+  it "email format should be valid" do
+    addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
+    addresses.each do |valid_address|
+      @user.email = valid_address
+      expect(@user).to be_valid
+    end
+  end
+
   it "email addresses should be saved as lower-case" do
     @user.email = "Foo@ExAMPle.CoM"
     @user.save!
