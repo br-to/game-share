e6d6da6fe940fbe1bf057b04aacb134f97fafa8d
raba <barabara0224@gmail.com>
2019-09-04 13:16:42 +0900

    move login user

--- spec/system/delete_comment_spec.rb
+++ spec/system/delete_comment_spec.rb
@@ -1,14 +1,15 @@
 require "rails_helper"
 
-describe "コメント削除" do
+describe "コメント削除", type: :system do
   let(:user) { create :user }
   let(:review) { create :review }
+
   before do
     create(:comment, user: user, review: review)
-    login user
   end
 
   it "コメント削除成功" do
+    login user
     visit review_path(review)
     click_link I18n.t("link.delete_comment")
     page.driver.browser.switch_to.alert.accept
