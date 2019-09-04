3f4055191c8bf0f0550fc272ffe353baca186587
raba <barabara0224@gmail.com>
2019-09-04 23:05:31 +0900

    integerate review& spoiler test

--- spec/system/delete_review_spec.rb
+++ spec/system/delete_review_spec.rb
@@ -3,15 +3,29 @@ require "rails_helper"
 describe "感想・レビュー削除", type: :system do
   let(:user) { create :user }
   let(:game) { create :game }
-  let(:review) { create :review, game: game, user: user }
-  before do
-    login user
+  describe "ネタバレなしレビュ―削除" do
+    let(:review) { create :review, game: game, user: user }
+
+    it "ネタバレなし感想・レビュー削除成功" do
+      login user
+      visit review_path(review)
+      click_link I18n.t("link.delete_review")
+      page.driver.browser.switch_to.alert.accept
+      expect(page).to have_content I18n.t("flash.review_destroy_success")
+      expect(page).to have_current_path game_path(game)
+    end
   end
 
-  it "感想・レビュー削除成功" do
-    visit review_path(review)
-    click_link I18n.t("link.delete_review")
-    page.driver.browser.switch_to.alert.accept
-    expect(page).to have_content I18n.t("flash.review_destroy_success")
+  describe "ネタバレありレビュー削除" do
+    let(:review) { create :review, is_spoiled: true, game: game, user: user }
+
+    it "ネタバレ削除成功" do
+      login user
+      visit review_path(review)
+      click_link I18n.t("link.delete_review")
+      page.driver.browser.switch_to.alert.accept
+      expect(page).to have_content I18n.t("flash.review_destroy_success")
+      expect(page).to have_current_path game_path(game)
+    end
   end
 end
--- spec/system/register_review_spec.rb
+++ spec/system/register_review_spec.rb
@@ -3,27 +3,58 @@ require "rails_helper"
 describe "感想・レビューの登録", type: :system do
   let(:user) { create :user }
   let(:game) { create :game, user: user }
-  before do
-    login user
-    visit new_game_review_path(game)
-    fill_in "review[content]", with: content
-    click_button I18n.t("title.register")
-  end
+  describe "ネタバレなしレビュ―登録" do
+    before do
+      login user
+      visit new_game_review_path(game)
+      fill_in "review[content]", with: content
+      click_button I18n.t("title.register")
+    end
 
-  context "正しい値を入力した場合" do
-    let(:content) { Faker::Lorem.sentence }
+    context "正しい値を入力した場合" do
+      let(:content) { Faker::Lorem.sentence }
 
-    it "感想・レビュー登録成功" do
-      expect(page).to have_content I18n.t("flash.review_registration_success")
-      expect(page).to have_content content
+      it "感想・レビュー登録成功" do
+        expect(page).to have_content I18n.t("flash.review_registration_success")
+        expect(page).to have_current_path game_path(game)
+        expect(page).to have_content content
+      end
+    end
+
+    context "不正なcontentを入力した場合" do
+      let(:content) { nil }
+
+      it "感想・レビュー登録失敗" do
+        expect(page).to have_content I18n.t("flash.registration_failed")
+      end
     end
   end
 
-  context "不正なcontentを入力した場合" do
-    let(:content) { nil }
+  describe "ネタバレありレビューの登録" do
+    before do
+      login user
+      visit new_game_review_path(game)
+      fill_in "review[content]", with: content
+      check "review[is_spoiled]"
+      click_button I18n.t("title.register")
+    end
 
-    it "感想・レビュー登録失敗" do
-      expect(page).to have_content I18n.t("flash.registration_failed")
+    context "正しい値を入力した場合" do
+      let(:content) { Faker::Lorem.sentence }
+
+      it "ネタバレ登録成功" do
+        expect(page).to have_content I18n.t("flash.spoiler_registration_success")
+        expect(page).to have_current_path game_reviews_path(game)
+        expect(page).to have_content content
+      end
+    end
+
+    context "不正なcontentを入力した場合" do
+      let(:content) { nil }
+
+      it "ネタバレ登録失敗" do
+        expect(page).to have_content I18n.t("flash.registration_failed")
+      end
     end
   end
 end
