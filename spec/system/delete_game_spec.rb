e591f05a5da492ff1f581c35e7a276c7a38d54cc
raba <barabara0224@gmail.com>
2019-09-04 23:06:03 +0900

    move login user

--- spec/system/delete_game_spec.rb
+++ spec/system/delete_game_spec.rb
@@ -3,11 +3,9 @@ require "rails_helper"
 describe "ゲームタイトル削除", type: :system do
   let(:user) { create :user }
   let(:game) { create :game, user: user }
-  before do
-    login user
-  end
 
   it "titleの削除成功" do
+    login user
     visit game_path(game)
     click_link I18n.t("link.delete_game")
     page.driver.browser.switch_to.alert.accept
