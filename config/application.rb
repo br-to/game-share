10456215fa9c70ab924cb21764f7e1393e1d78d0
raba <barabara0224@gmail.com>
2019-09-09 23:06:50 +0900

    delete content_tag

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,6 @@ module GameShare
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.i18n.default_locale = :ja
-    config.action_view.field_error_proc = proc do |html_tag, _instance|
-      content_tag(html_tag.to_s)
-    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
