58d0b78ba187e458a2c68d7ececca406b9a8fe2e
raba <barabara0224@gmail.com>
2019-09-09 15:20:30 +0900

    define db posgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_08_19_050747) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "comments", force: :cascade do |t|
     t.integer "user_id"
     t.integer "review_id"
