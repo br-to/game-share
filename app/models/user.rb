567a21df4edf3e4f0a9d8c6592feff4a641c9701
raba <barabara0224@gmail.com>
2019-09-11 22:29:10 +0900

    add picture_uploader

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
   validates :password, presence: true, length: { minimum: 6 }
   has_secure_password
+  mount_uploader :user_picture, PictureUploader
 
   private
 
