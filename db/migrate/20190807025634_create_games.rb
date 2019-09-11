0ccb728f1f7fddef0f7988c87e5cbf87b556a3ac
raba <barabara0224@gmail.com>
2019-09-11 22:39:05 +0900

    add column *_picture

--- db/migrate/20190807025634_create_games.rb
+++ db/migrate/20190807025634_create_games.rb
@@ -3,6 +3,8 @@ class CreateGames < ActiveRecord::Migration[5.2]
     create_table :games do |t|
       t.string :title, index: true
       t.integer :user_id, index: true
+      t.string :game_picture
+
       t.timestamps
     end
   end
