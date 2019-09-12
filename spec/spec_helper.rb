cc5cca53956fb74bc7959ba34a44d35813bcaea2
raba <barabara0224@gmail.com>
2019-09-13 01:07:40 +0900

    add FileUtils

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,6 +17,12 @@ RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
+  config.after(:all) do
+    if Rails.env.test?
+      FileUtils.rm_rf(Dir["#{Rails.root}/public/uploads/"])
+    end
+  end
+  # put logic in this file or initializer/carrierwave.rb
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4. It makes the `description`
     # and `failure_message` of custom matchers include text for helper methods
