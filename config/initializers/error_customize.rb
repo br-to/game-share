0bcf14fbf61225350582dacdc81d472f82f789f9
raba <barabara0224@gmail.com>
2019-09-17 22:03:26 +0900

    fix error_customize

--- config/initializers/error_customize.rb
+++ config/initializers/error_customize.rb
@@ -1,18 +1,19 @@
 ActionView::Base.field_error_proc = proc do |html_tag, instance|
   if instance.is_a?(ActionView::Helpers::Tags::Label)
-    html_tag.html_safe
+    html_tag
   else
     method_name = instance.instance_variable_get(:@method_name)
     errors = instance.object.errors[method_name]
-    html = <<~EOM
-      <div class="has-error">
-        #{html_tag}
-        <span class="help-block">
-          #{I18n.t("activerecord.attributes.#{instance.object.class.name.underscore}.#{method_name}")}
-          #{errors.first}
-        </span>
-      </div>
-    EOM
-    html.html_safe
+    view = ActionView::Base.new
+    field_tag = []
+    field_tag << html_tag
+    field_tag += errors.map do |error|
+      view.content_tag(:div, class: "help-block") do
+        "#{instance.object.class.human_attribute_name(method_name)}#{error}"
+      end
+    end
+    view.content_tag(:div, class: "has-error") do
+      view.safe_join(field_tag)
+    end
   end
 end
