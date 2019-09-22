176d6af2fc3a32d192bd9576df328c118297780d
raba <barabara0224@gmail.com>
2019-09-22 21:30:43 +0900

    add config.action_mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,18 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "game_share_#{Rails.env}"
-
+  config.action_mailer.delivery_method = :smtp
+  host = "https://game-share-1.herokuapp.com"
+  config.action.mailer.default_url_options = { host: host }
+  ActionMailer::Base.smtp_settings = {
+    address: "smtp.sendgrid.net",
+    port: "587",
+    authentication: :plain,
+    user_name: ENV["SENDGRID_USERNAME"],
+    password: ENV["SENDGRID_PASSWORD"],
+    domain: "heroku.com",
+    enable_starttle_auto: true,
+  }
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
