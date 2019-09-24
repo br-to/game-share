1bc7cc1044c0eb83ac60a519455de22a7d2acff2
raba <barabara0224@gmail.com>
2019-09-25 01:10:10 +0900

    fix authenticate test

--- spec/system/user_authenticate_spec.rb
+++ spec/system/user_authenticate_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 describe "認証機能", type: :system do
   let(:user) { create :user }
-  describe "ログイン" do
+  describe "アカウントを有効化してからログイン" do
     before do
       activate user
       visit login_path
@@ -12,12 +12,11 @@ describe "認証機能", type: :system do
     end
 
     context "正しい値を入力した場合" do
-      let(:user) { create :user }
       let(:email) { user.email }
       let(:password) { user.password }
 
       it "ログイン成功" do
-        # expect(page).to have_content I18n.t("flash.account_activation")
+        expect(page).to have_content I18n.t("flash.login_success")
         expect(page).to have_current_path profile_path
         expect(page).to have_content user.name
         expect(page).to have_content user.email
@@ -34,7 +33,6 @@ describe "認証機能", type: :system do
     end
 
     context "登録したユーザーのemailと不正passwordを入力した場合" do
-      let(:user) { create :user }
       let(:email) { user.email }
       let(:password) { "foobar" }
 
@@ -53,9 +51,26 @@ describe "認証機能", type: :system do
     end
   end
 
-  describe "ログアウト" do
-    let(:user) { create(:user) }
+  describe "アカウントを有効化せずログイン" do
+    before do
+      visit login_path
+      fill_in "session[email]", with: email
+      fill_in "session[password]", with: password
+      click_button I18n.t("title.login")
+    end
 
+    context "正しい値を入力した場合" do
+      let(:email) { user.email }
+      let(:password) { user.password }
+
+      it "ログイン失敗" do
+        expect(page).to have_content I18n.t("flash.login_failed")
+        expect(page).to have_current_path root_path
+      end
+    end
+  end
+
+  describe "ログアウト" do
     it "ログアウト成功" do
       activate user
       login user
