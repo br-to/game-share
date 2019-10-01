a214a7d2e16071b6053c833a18f14a294a1796f3
raba <barabara0224@gmail.com>
2019-10-01 20:22:29 +0900

    delete @like

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,7 +8,6 @@ class CommentsController < ApplicationController
       redirect_to review_url(@review), success: t(:comment_registration_success, scope: :flash)
     else
       flash.now[:warning] = t(:registration_failed, scope: :flash)
-      @like = @review.likes.find_by(user: current_user)
       @comments = @review.comments.includes(:user)
       render "reviews/show"
     end
