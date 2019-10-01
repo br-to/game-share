d69828cdca3ceff44c352a96cb622d6f592286dd
raba <barabara0224@gmail.com>
2019-10-01 12:57:46 +0900

    fix like_spec

--- spec/system/like_spec.rb
+++ spec/system/like_spec.rb
@@ -2,17 +2,17 @@ require "rails_helper"
 
 describe "ナイス機能", type: :system do
   describe "感想・レビューをナイスする" do
+    let(:user) { create :user }
+    let(:review) { create(:review, user: user) }
     let(:mail) { LikeMailer.liked_review(user) }
     before do
-      user = create(:user)
-      review = create(:review, user: user)
       activate user
       login user
       visit review_path(review)
+      click_link I18n.t("link.register_like")
     end
 
     it "ナイス成功" do
-      click_link I18n.t("link.register_like")
       expect(page).to have_content I18n.t("link.delete_like")
       expect(page).to have_content I18n.t("message.likes_count", count: 1)
     end
