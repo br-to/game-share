906cd0675ede88a8e723e180520646f442b87cdc
raba <barabara0224@gmail.com>
2019-10-02 13:46:12 +0900

    delete id

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,12 +2,13 @@ class CommentsController < ApplicationController
   def create
     @review = Review.find(params[:review_id])
     @comment = @review.comments.build(params_comment)
-    @comment.user_id = current_user.id
+    @comment.user = current_user
     if @comment.save
       CommentMailer.commented_review(@review.user, @review, @comment).deliver_now
       redirect_to review_url(@review), success: t(:comment_registration_success, scope: :flash)
     else
       flash.now[:warning] = t(:registration_failed, scope: :flash)
+      @like = @review.likes.find_by(user: current_user)
       @comments = @review.comments.includes(:user)
       render "reviews/show"
     end
