f0f6f2d5f868ec9267cc387310d14d98fb790127
raba <barabara0224@gmail.com>
2019-10-29 16:18:33 +0900

    fix controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,6 @@
 class GamesController < ApplicationController
   def index
-    @games = Game.order(created_at: :desc).page params[:page]
+    @games = Game.order(created_at: :desc).includes(:user).page params[:page]
   end
 
   def new
