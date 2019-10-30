d2d210460259264b0f63032ccd1b4996a1ae9bf5
raba <barabara0224@gmail.com>
2019-10-31 01:22:03 +0900

    add if sentence

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,12 +1,13 @@
-CarrierWave.configure do |config|
-  config.cache_storage = :fog
-  config.fog_provider = "fog/aws"
-  config.fog_credentials = {
-    provider: "AWS",
-    aws_access_key_id: ENV["S3_ACCESS_KEY_ID"],
-    aws_secret_access_key: ENV["S3_SECRET_ACCESS_KEY"],
-    region: "ap-northeast-1",
-  }
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.cache_storage = :fog
+    config.fog_credentials = {
+      provider: "AWS",
+      aws_access_key_id: ENV["S3_ACCESS_KEY_ID"],
+      aws_secret_access_key: ENV["S3_SECRET_ACCESS_KEY"],
+      region: "ap-northeast-1",
+    }
 
-  config.fog_directory = "game-share-first-bucket"
+    config.fog_directory = "game-share-first-bucket"
+  end
 end
