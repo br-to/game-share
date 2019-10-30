1bce50b8831c962afb3bb17cf18d7baa05b76481
raba <barabara0224@gmail.com>
2019-10-31 01:04:07 +0900

    set access_key & secret_key

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,8 +3,8 @@ CarrierWave.configure do |config|
   config.fog_provider = "fog/aws"
   config.fog_credentials = {
     provider: "AWS",
-    aws_access_key_id: Rails.application.secrets.aws_access_key_id,
-    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
+    aws_access_key_id: ENV["S3_ACCESS_KEY_ID"],
+    aws_secret_access_key: ENV["S3_SECRET_ACCESS_KEY"],
     region: "ap-northeast-1",
   }
 
