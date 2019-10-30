46b4b02b1ab3068a3c04618b13e57da73902e38f
raba <barabara0224@gmail.com>
2019-10-31 00:11:09 +0900

    add fog_provider

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,12 @@
 CarrierWave.configure do |config|
+  config.cache_storage = :fog
+  config.fog_provider = "fog/aws"
   config.fog_credentials = {
     provider: "AWS",
-    aws_access_key_id: ENV["S3_ACCESS_KEY"],
-    aws_secret_access_key: ENV["S3_SECRET_KEY"],
+    aws_access_key_id: Rails.application.secrets.aws_access_key_id,
+    aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
     region: "ap-northeast-1",
   }
 
   config.fog_directory = "game-share-first-bucket"
-  config.cache_storage = :fog
 end
