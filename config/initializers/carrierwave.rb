fa4083ed52cf80a9ba8b36c9ded4a4b721ac4741
raba <barabara0224@gmail.com>
2019-10-31 15:00:48 +0900

    fix carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,13 +1,20 @@
-if Rails.env.production?
-  CarrierWave.configure do |config|
+require "carrierwave/storage/abstract"
+require "carrierwave/storage/file"
+require "carrierwave/storage/fog"
+
+CarrierWave.configure do |config|
+  if Rails.env.production?
     config.cache_storage = :fog
+    config.fog_provider = "fog/aws"
+    config.fog_directory = "game-share-first-bucket"
     config.fog_credentials = {
       provider: "AWS",
       aws_access_key_id: ENV["S3_ACCESS_KEY_ID"],
       aws_secret_access_key: ENV["S3_SECRET_ACCESS_KEY"],
       region: "ap-northeast-1",
     }
-
-    config.fog_directory = "game-share-first-bucket"
+  else
+    config.storage :file
+    config.enable_processing = false if Rails.env.test?
   end
 end
